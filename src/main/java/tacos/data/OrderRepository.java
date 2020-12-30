b74c1da9a6a6b0b5917e5b7d5a2c36fb0d955e89
Rusu Tiberiu <rusu_tiberiu15@yahoo.com>
2020-12-31 00:49:49 +0200

    First version of Taco Cloud

---
+++ src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,21 @@
+package tacos.data;
+
+import org.aspectj.weaver.ast.Or;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import tacos.Order;
+import tacos.User;
+
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    List<Order> findByZip(String zip);
+
+    List<Order> readOrderByZipAndPlacedAtBetween(String zip, Date startDate, Date endDate);
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
+
+//    Order save(Order order);
+}
